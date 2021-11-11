2787b32b68ec0e19bbb4cdc5f51ad44803e35d87
Mohit Malviya <9685mohit@gmail.com>
2021-11-11 15:27:21 +0530

    updated Expection Handling Code

--- Encrypt.py
+++ Encrypt.py
@@ -1,14 +1,25 @@
 """Enrypting text with Fernet"""
 from cryptography.fernet import Fernet
+import pyttsx3
+from pyttsx3 import engine
 
-text = "This is a secret message"
-#generate key
-key = Fernet.generate_key()
-#creating instance 
-fernet = Fernet(key)
 
-#using fernet class instance to encrypt string 
-encMessage = fernet.encrypt(text.encode())
-#print
-print(f"message before encryption:{text}")
-print(f"message after encryption:{encMessage}")
+try :
+    print("Cypher is Running...")
+    # text = "This is a secret message"
+    text = str(input("Enter the text you want to encrypt:"))
+    #generate key
+    key = Fernet.generate_key()
+    #creating instance 
+    fernet = Fernet(key)
+
+    #using fernet class instance to encrypt string 
+    encMessage = fernet.encrypt(text.encode())
+    #print
+    print(f"message before encryption: {text}")
+    print(f"message after encryption: {encMessage}")
+except Exception as error:
+    engine= pyttsx3.init()
+    engine.say(error)
+    print("Error:",error)
+    engine.runAndWait()
